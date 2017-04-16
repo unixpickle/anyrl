d083a212fb917df14d3440d7f9377c00b1aa4ec9
Alex Nichol <unixpickle@gmail.com>
2017-04-16 11:08:16 -0400

    create separate action_space file

--- action_space.go
+++ action_space.go
@@ -1,7 +1,3 @@
-// Package trpo implements Trust Region Policy
-// Optimization, a gradient-based reinforcement learning
-// algorithm.
-// See https://arxiv.org/abs/1502.05477.
 package trpo
 
 import (
---
+++ doc.go
@@ -0,0 +1,5 @@
+// Package trpo implements Trust Region Policy
+// Optimization, a gradient-based reinforcement learning
+// algorithm.
+// See https://arxiv.org/abs/1502.05477.
+package trpo
