503af699b41b825dbba7df5df16f3151a1e0eb7b
Alex Nichol <unixpickle@gmail.com>
2017-04-17 13:58:52 -0400

    fisher info matrix passes tests
    Now I truly have no idea what's wrong with the conjugate gradient setup.

--- natural_grad_test.go
+++ natural_grad_test.go
@@ -1,6 +1,7 @@
 package anyrl
 
 import (
+	"math"
 	"testing"
 
 	"github.com/unixpickle/anydiff"
@@ -11,6 +12,85 @@ import (
 	"github.com/unixpickle/anyvec/anyvec64"
 	"github.com/unixpickle/lazyrnn"
 )
+
+func TestFisherDeterministic(t *testing.T) {
+	c := anyvec64.DefaultCreator{}
+	r := rolloutsForTest(c)
+
+	block := &anyrnn.LayerBlock{
+		Layer: anynet.Net{
+			anynet.NewFC(c, 3, 2),
+			anynet.Tanh,
+			anynet.NewFC(c, 2, 2),
+		},
+	}
+
+	npg := &NaturalPG{
+		Policy:      block,
+		Params:      block.Parameters(),
+		ActionSpace: Softmax{},
+		Iters:       14,
+	}
+
+	inGrad := anydiff.NewGrad(block.Parameters()...)
+	for _, vec := range inGrad {
+		anyvec.Rand(vec, anyvec.Normal, nil)
+	}
+	stored := npg.storePolicyOutputs(c, r)
+
+	grad1 := npg.applyFisher(r, inGrad, stored)
+	mag1 := dotGrad(grad1, grad1).(float64)
+	for i := 0; i < 1000; i++ {
+		grad2 := npg.applyFisher(r, inGrad, stored)
+		mag2 := dotGrad(grad2, grad2).(float64)
+		correlation := dotGrad(grad1, grad2).(float64) / math.Sqrt(mag1*mag2)
+		if correlation < 1-1e-3 {
+			t.Errorf("correlation is too low: %f", correlation)
+		}
+	}
+}
+
+func TestFisher(t *testing.T) {
+	c := anyvec64.DefaultCreator{}
+	r := rolloutsForTest(c)
+
+	block := &anyrnn.LayerBlock{
+		Layer: anynet.Net{
+			anynet.NewFC(c, 3, 2),
+			anynet.Tanh,
+			anynet.NewFC(c, 2, 2),
+		},
+	}
+
+	npg := &NaturalPG{
+		Policy:      block,
+		Params:      block.Parameters(),
+		ActionSpace: Softmax{},
+		Iters:       14,
+	}
+
+	inGrad := anydiff.NewGrad(block.Parameters()...)
+	for _, vec := range inGrad {
+		anyvec.Rand(vec, anyvec.Normal, nil)
+		vec.Scale(c.MakeNumeric(0.0001))
+	}
+	stored := npg.storePolicyOutputs(c, r)
+
+	applied := npg.applyFisher(r, inGrad, stored)
+	actualOutput := 0.5 * dotGrad(inGrad, applied).(float64)
+
+	inGrad.AddToVars()
+	outSeq := npg.apply(lazyrnn.TapeRereader(c, r.Inputs), block)
+	mapped := lazyrnn.MapN(func(num int, v ...anydiff.Res) anydiff.Res {
+		return npg.ActionSpace.KL(v[0], v[1], num)
+	}, lazyrnn.TapeRereader(c, stored), outSeq)
+	expectedOutput := anyvec.Sum(lazyrnn.Mean(mapped).Output()).(float64)
+
+	diff := (actualOutput - expectedOutput) / actualOutput
+	if math.Abs(diff) > 1e-3 {
+		t.Errorf("expected %v but got %v", expectedOutput, actualOutput)
+	}
+}
 
 func TestConjugateGradients(t *testing.T) {
 	c := anyvec64.DefaultCreator{}
