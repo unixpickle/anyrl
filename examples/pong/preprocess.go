79cd1b0d558be11fe9e5c38d5e3d116e95314c47
Alex Nichol <unixpickle@gmail.com>
2017-04-19 15:24:07 -0400

    fixed preprocessing layer for anyfwd
    When training got up to TRPO, the program panic()'d because of using the mapper on a different creator type.

--- examples/pong/preprocess.go
+++ examples/pong/preprocess.go
@@ -1,15 +1,10 @@
 package main
 
 import (
-	"sync"
-
 	"github.com/unixpickle/anydiff"
 	"github.com/unixpickle/anyvec"
 	"github.com/unixpickle/serializer"
 )
-
-var inputSubsamplerLock sync.Mutex
-var inputSubsampler anyvec.Mapper
 
 const (
 	FrameWidth  = 160
@@ -30,13 +25,16 @@ func DeserializePreprocessLayer(d []byte) (PreprocessLayer, error) {
 }
 
 func (p PreprocessLayer) Apply(in anydiff.Res, n int) anydiff.Res {
+	cr := in.Output().Creator()
+	subsampler := makeInputSubsampler(cr)
 	imgSize := FrameWidth * FrameHeight * 3
 	var subsampled []anyvec.Vector
 	for i := 0; i < n; i++ {
-		img := preprocessImage(in.Output().Slice(i*imgSize, (i+1)*imgSize))
-		subsampled = append(subsampled, img)
+		inImg := in.Output().Slice(i*imgSize, (i+1)*imgSize)
+		outImg := preprocessImage(subsampler, inImg)
+		subsampled = append(subsampled, outImg)
 	}
-	return anydiff.NewConst(in.Output().Creator().Concat(subsampled...))
+	return anydiff.NewConst(cr.Concat(subsampled...))
 }
 
 func (p PreprocessLayer) SerializerType() string {
@@ -47,16 +45,15 @@ func (p PreprocessLayer) Serialize() ([]byte, error) {
 	return []byte{}, nil
 }
 
-func preprocessImage(image anyvec.Vector) anyvec.Vector {
+func preprocessImage(sampler anyvec.Mapper, image anyvec.Vector) anyvec.Vector {
 	// Logic taken from:
 	// https://gist.github.com/karpathy/a4166c7fe253700972fcbc77e4ea32c5#file-pg-pong-py-L32.
 
 	cr := image.Creator()
 
 	// Crop image to 80x80
-	subsampler := getInputSubsampler(cr)
-	out := cr.MakeVector(subsampler.OutSize())
-	subsampler.Map(image, out)
+	out := cr.MakeVector(sampler.OutSize())
+	sampler.Map(image, out)
 
 	// Find background pixels
 	bg1 := out.Copy()
@@ -70,24 +67,17 @@ func preprocessImage(image anyvec.Vector) anyvec.Vector {
 	return out
 }
 
-func getInputSubsampler(cr anyvec.Creator) anyvec.Mapper {
-	inputSubsamplerLock.Lock()
-	defer inputSubsamplerLock.Unlock()
-
-	if inputSubsampler == nil {
-		// Creating a mapping which does the following:
-		//  - crop box from x=35 to x=195
-		//  - subsample by factor of 2
-		//  - select the red channel
-		mapping := make([]int, 0, 80*80)
-		for y := 35; y < 195; y += 2 {
-			for x := 0; x < FrameWidth; x += 2 {
-				sourceIdx := y*FrameWidth*3 + x*3
-				mapping = append(mapping, sourceIdx)
-			}
+func makeInputSubsampler(cr anyvec.Creator) anyvec.Mapper {
+	// Creating a mapping which does the following:
+	//  - crop box from x=35 to x=195
+	//  - subsample by factor of 2
+	//  - select the red channel
+	mapping := make([]int, 0, 80*80)
+	for y := 35; y < 195; y += 2 {
+		for x := 0; x < FrameWidth; x += 2 {
+			sourceIdx := y*FrameWidth*3 + x*3
+			mapping = append(mapping, sourceIdx)
 		}
-		inputSubsampler = cr.MakeMapper(FrameWidth*FrameHeight*3, mapping)
 	}
-
-	return inputSubsampler
+	return cr.MakeMapper(FrameWidth*FrameHeight*3, mapping)
 }
