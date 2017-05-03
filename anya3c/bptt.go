5afb152cf13594686ad137fb96450646e5729725
Alex Nichol <unixpickle@gmail.com>
2017-05-03 13:07:51 -0400

    drafted A3C bptt and rollout

---
+++ anya3c/bptt.go
@@ -0,0 +1,77 @@
+package anya3c
+
+import (
+	"github.com/unixpickle/anydiff"
+	"github.com/unixpickle/anynet/anyrnn"
+	"github.com/unixpickle/anyrl/anypg"
+	"github.com/unixpickle/anyvec"
+)
+
+// bptt implements back-propagation through time.
+type bptt struct {
+	Rollout     *rollout
+	Worker      *worker
+	Discount    float64
+	Regularizer anypg.Regularizer
+}
+
+// Run performs back-propagation through time.
+//
+// Since BPTT may be bootstrapped, the worker may be used
+// to run the critic on the next observation.
+func (b *bptt) Run() anydiff.Grad {
+	grad := anydiff.NewGrad(b.Worker.Agent.Params...)
+
+	if len(b.Worker.Agent.Params) == 0 {
+		return grad
+	}
+
+	advantages := b.Rollout.Advantages(b.Worker, b.Discount)
+
+	c := b.Worker.Agent.Params[0].Vector.Creator()
+	ops := c.NumOps()
+
+	stateUpstream := make([]anyrnn.StateGrad, 3)
+	for t := len(advantages) - 1; t >= 0; t-- {
+		outReses := b.Rollout.Outs[t]
+		advantage := advantages[t]
+
+		criticUpstream := c.MakeVector(1)
+		criticUpstream.AddScalar(ops.Mul(advantage, c.MakeNumeric(2)))
+		actorUpstream := b.actorUpstream(outReses[1].Output(),
+			b.Rollout.Sampled[t], advantage)
+
+		var baseUpstream1, baseUpstream2 anyvec.Vector
+		baseUpstream1, stateUpstream[1] = outReses[1].Propagate(actorUpstream,
+			stateUpstream[1], grad)
+		baseUpstream2, stateUpstream[2] = outReses[2].Propagate(criticUpstream,
+			stateUpstream[2], grad)
+
+		baseUpstream1.Add(baseUpstream2)
+
+		_, stateUpstream[0] = outReses[0].Propagate(baseUpstream1,
+			stateUpstream[0], grad)
+	}
+
+	return grad
+}
+
+func (b *bptt) actorUpstream(params, sampled anyvec.Vector,
+	advantage anyvec.Numeric) anyvec.Vector {
+	c := params.Creator()
+	paramVar := anydiff.NewVar(params)
+	grad := anydiff.NewGrad(paramVar)
+
+	upstream := c.MakeVector(1)
+	upstream.AddScalar(advantage)
+	logProb := b.Worker.Agent.ActionSpace.LogProb(paramVar, sampled, 1)
+	logProb.Propagate(upstream, grad)
+
+	if b.Regularizer != nil {
+		penalty := b.Regularizer.Regularize(paramVar, 1)
+		upstream.SetData(c.MakeNumericList([]float64{1}))
+		penalty.Propagate(upstream, grad)
+	}
+
+	return grad[paramVar]
+}
---
+++ anya3c/rollout.go
@@ -0,0 +1,71 @@
+package anya3c
+
+import (
+	"github.com/unixpickle/anynet/anyrnn"
+	"github.com/unixpickle/anyvec"
+)
+
+// rollout represents a (partial) trajectory through an
+// environment.
+type rollout struct {
+	Outs    [][]anyrnn.Res
+	Rewards []float64
+	Sampled []anyvec.Vector
+}
+
+// runRollout generates a rollout by sampling actions
+// using a worker.
+//
+// If maxSteps is non-zero, it limits the number of
+// timesteps in the environment.
+func runRollout(w *worker, maxSteps int) (*rollout, error) {
+	var r rollout
+	for t := 0; t < maxSteps || maxSteps == 0; t++ {
+		w.StepAgent()
+		lastOut := w.AgentRes
+		reward, action, err := w.StepEnv()
+		if err != nil {
+			return nil, err
+		}
+		r.add(lastOut, reward, action)
+		if w.EnvDone {
+			break
+		}
+	}
+	return &r, nil
+}
+
+// Advantages computes an empirical advantage function
+// estimator at every timestep.
+//
+// Since this may be bootstrapped, the worker may be used
+// to run the critic on the next observation.
+func (r *rollout) Advantages(w *worker, discount float64) []anyvec.Numeric {
+	c := w.Agent.Params[0].Vector.Creator()
+	ops := c.NumOps()
+
+	followingReward := c.MakeNumeric(0)
+	if !w.EnvDone {
+		// Bootstrap from value function.
+		w.StepAgent()
+		followingReward = anyvec.Sum(w.AgentRes[2].Output())
+	}
+	discountNum := c.MakeNumeric(discount)
+
+	advantages := make([]anyvec.Numeric, len(r.Rewards))
+	for t := len(r.Rewards) - 1; t >= 0; t-- {
+		followingReward = ops.Add(c.MakeNumeric(r.Rewards[t]),
+			ops.Mul(discountNum, followingReward))
+		criticOut := anyvec.Sum(r.Outs[t][2].Output())
+		advantages[t] = ops.Sub(followingReward, criticOut)
+	}
+
+	return advantages
+}
+
+// add appends a frame of experience.
+func (r *rollout) add(out []anyrnn.Res, reward float64, sampled anyvec.Vector) {
+	r.Outs = append(r.Outs, out)
+	r.Rewards = append(r.Rewards, reward)
+	r.Sampled = append(r.Sampled, sampled)
+}
