c7c38e040df5a531f868c06ad6e90cfafca7b82f
Alex Nichol <unixpickle@gmail.com>
2017-06-06 10:29:57 -0400

    drafted Slave proxy system
    This will make it possible to coordinate slaves running all over the internet.

---
+++ anyes/proxy.go
@@ -0,0 +1,227 @@
+package anyes
+
+import (
+	"encoding/gob"
+	"errors"
+	"fmt"
+	"io"
+	"time"
+
+	"github.com/unixpickle/essentials"
+	"github.com/unixpickle/gobplexer"
+)
+
+func init() {
+	gob.Register(&packet{})
+}
+
+const (
+	keepaliveInterval = time.Minute
+	keepaliveMaxDelay = time.Minute * 2
+)
+
+type packetType int
+
+const (
+	packetInit packetType = iota
+	packetRun
+	packetUpdate
+)
+
+type packet struct {
+	Type packetType
+
+	// Used for init requests.
+	InitSeed  int64
+	InitSize  int
+	InitModel []byte
+
+	// Used for run requests.
+	Stop  *StopConds
+	Scale float64
+	Seed  int64
+
+	// Used for run responses.
+	Rollout *Rollout
+
+	// Used for update requests.
+	Scales []float64
+	Seeds  []int64
+
+	// Used for all responses.
+	Err *string
+}
+
+func newPacketErr(err error) *packet {
+	if err == nil {
+		return &packet{}
+	}
+	s := err.Error()
+	return &packet{Err: &s}
+}
+
+// ProxyProvide provides a Slave to the other end of a
+// proxy, which should be using ProxyConsume.
+//
+// This blocks until the proxy connection ends.
+// It automatically closes c.
+func ProxyProvide(c io.ReadWriteCloser, s Slave) (err error) {
+	defer essentials.AddCtxTo("provide proxy", &err)
+
+	rootConn := gobplexer.NetConnection(c)
+	defer rootConn.Close()
+
+	connector := gobplexer.MultiplexConnector(rootConn)
+	defer connector.Close()
+
+	conn, err := gobplexer.KeepaliveConnector(connector, keepaliveInterval,
+		keepaliveMaxDelay)
+	if err != nil {
+		return err
+	}
+	defer conn.Close()
+
+	for {
+		p, err := receivePacket(conn)
+		if err != nil {
+			return err
+		}
+		switch p.Type {
+		case packetInit:
+			err := s.Init(p.InitModel, p.InitSeed, p.InitSize)
+			err = conn.Send(newPacketErr(err))
+			if err != nil {
+				return err
+			}
+		case packetRun:
+			rollout, err := s.Run(p.Stop, p.Scale, p.Seed)
+			packet := newPacketErr(err)
+			packet.Rollout = rollout
+			err = conn.Send(newPacketErr(err))
+			if err != nil {
+				return err
+			}
+		case packetUpdate:
+			err := s.Update(p.Scales, p.Seeds)
+			err = conn.Send(newPacketErr(err))
+			if err != nil {
+				return err
+			}
+		default:
+			return fmt.Errorf("unknown packet type: %v", p.Type)
+		}
+	}
+}
+
+// SlaveProxy is a connection to a remote Slave.
+//
+// A SlaveProxy should be closed to clean up resources
+// associated with it.
+type SlaveProxy interface {
+	io.Closer
+	Slave
+}
+
+type slaveProxy struct {
+	closers []io.Closer
+	conn    gobplexer.Connection
+}
+
+// ProxyConsume connects to a Slave proxy which is running
+// ProxyProvide on the other end.
+func ProxyConsume(c io.ReadWriteCloser) (slave SlaveProxy, err error) {
+	defer essentials.AddCtxTo("consume proxy", &err)
+
+	res := &slaveProxy{}
+
+	rootConn := gobplexer.NetConnection(c)
+	res.closers = append(res.closers, rootConn)
+
+	connector := gobplexer.MultiplexConnector(rootConn)
+	res.closers = append(res.closers, connector)
+
+	conn, err := gobplexer.KeepaliveConnector(connector, keepaliveInterval,
+		keepaliveMaxDelay)
+	if err != nil {
+		res.Close()
+		return nil, err
+	}
+	res.closers = append(res.closers, conn)
+	res.conn = conn
+
+	return res, nil
+}
+
+func (r *slaveProxy) Close() error {
+	for _, c := range r.closers {
+		c.Close()
+	}
+	return nil
+}
+
+func (s *slaveProxy) Init(data []byte, seed int64, size int) (err error) {
+	defer essentials.AddCtxTo("slave proxy init", &err)
+	return s.call(&packet{
+		Type:      packetInit,
+		InitModel: data,
+		InitSeed:  seed,
+		InitSize:  size,
+	})
+}
+
+func (s *slaveProxy) Run(sc *StopConds, scale float64, seed int64) (r *Rollout,
+	err error) {
+	defer essentials.AddCtxTo("slave proxy run", &err)
+	p := &packet{
+		Type:  packetRun,
+		Stop:  sc,
+		Scale: scale,
+		Seed:  seed,
+	}
+	if err := s.conn.Send(p); err != nil {
+		return nil, err
+	}
+	p, err = receivePacket(s.conn)
+	if err != nil {
+		return nil, err
+	}
+	if p.Err != nil {
+		return p.Rollout, errors.New(*p.Err)
+	}
+	return p.Rollout, nil
+}
+
+func (s *slaveProxy) Update(scales []float64, seeds []int64) (err error) {
+	defer essentials.AddCtxTo("slave proxy update", &err)
+	return s.call(&packet{
+		Type:   packetInit,
+		Scales: scales,
+		Seeds:  seeds,
+	})
+}
+
+func (s *slaveProxy) call(p *packet) error {
+	if err := s.conn.Send(p); err != nil {
+		return err
+	}
+	p, err := receivePacket(s.conn)
+	if err != nil {
+		return err
+	}
+	if p.Err != nil {
+		return errors.New(*p.Err)
+	}
+	return nil
+}
+
+func receivePacket(c gobplexer.Connection) (*packet, error) {
+	packetObj, err := c.Receive()
+	if err != nil {
+		return nil, err
+	}
+	packet, ok := packetObj.(*packet)
+	if !ok {
+		return nil, fmt.Errorf("bad packet type: %T", packetObj)
+	}
+	return packet, nil
+}
