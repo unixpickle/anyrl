5b8f29235baae8c6976f85f1e8d5ddbff3ab46b6
Alex Nichol <unixpickle@gmail.com>
2017-08-12 16:55:15 -0400

    added option to pool base output in PPO
    This could potentially lead to a 2x speedup when the base is a convolutional neural network.

--- anypg/ppo.go
+++ anypg/ppo.go
@@ -2,6 +2,7 @@ package anypg
 
 import (
 	"github.com/unixpickle/anydiff"
+	"github.com/unixpickle/anydiff/anyseq"
 	"github.com/unixpickle/anynet"
 	"github.com/unixpickle/anyrl"
 	"github.com/unixpickle/anyvec"
@@ -64,6 +65,14 @@ type PPO struct {
 	//
 	// If 0, DefaultPPOEpsilon is used.
 	Epsilon float64
+
+	// PoolBase, if true, indicates that the output of the
+	// Base function should be pooled to prevent multiple
+	// forward/backward Base evaluations.
+	//
+	// If this is true, then the entire output of Base is
+	// stored in memory.
+	PoolBase bool
 }
 
 // Advantage computes the GAE estimator for a batch.
@@ -128,54 +137,51 @@ func (p *PPO) Run(r *anyrl.RolloutSet, adv lazyseq.Tape) (anydiff.Grad, *PPOTerm
 		return grad, nil
 	}
 	c := creatorFromGrad(grad)
-
-	criticOut := p.Critic(p.applyBase(c, r))
-	actorOut := p.Actor(p.applyBase(c, r))
-
 	targetValues := (&QJudger{Discount: p.Discount}).JudgeActions(r)
 
-	obj := lazyseq.MapN(
-		func(n int, v ...anydiff.Res) anydiff.Res {
-			actor, critic := v[0], v[1]
-			oldOuts, actions := v[2], v[3]
-			advantage, targets := v[4], v[5]
+	objective := p.runActorCritic(c, r, func(actor, critic lazyseq.Rereader) anydiff.Res {
+		obj := lazyseq.MapN(
+			func(n int, v ...anydiff.Res) anydiff.Res {
+				actor, critic := v[0], v[1]
+				oldOuts, actions := v[2], v[3]
+				advantage, targets := v[4], v[5]
 
-			ratios := anydiff.Exp(
-				anydiff.Sub(
-					p.ActionSpace.LogProb(actor, actions.Output(), n),
-					p.ActionSpace.LogProb(oldOuts, actions.Output(), n),
-				),
-			)
-			advTerm := p.clippedObjective(ratios, advantage)
+				ratios := anydiff.Exp(
+					anydiff.Sub(
+						p.ActionSpace.LogProb(actor, actions.Output(), n),
+						p.ActionSpace.LogProb(oldOuts, actions.Output(), n),
+					),
+				)
+				advTerm := p.clippedObjective(ratios, advantage)
 
-			criticCoeff := -1.0
-			if p.CriticWeight != 0 {
-				criticCoeff *= p.CriticWeight
-			}
-			criticTerm := anydiff.Scale(
-				anydiff.Square(anydiff.Sub(critic, targets)),
-				c.MakeNumeric(criticCoeff),
-			)
+				criticCoeff := -1.0
+				if p.CriticWeight != 0 {
+					criticCoeff *= p.CriticWeight
+				}
+				criticTerm := anydiff.Scale(
+					anydiff.Square(anydiff.Sub(critic, targets)),
+					c.MakeNumeric(criticCoeff),
+				)
 
-			var regTerm anydiff.Res
-			if p.Regularizer != nil {
-				regTerm = p.Regularizer.Regularize(actor, n)
-			} else {
-				regTerm = anydiff.NewConst(c.MakeVector(n))
-			}
+				var regTerm anydiff.Res
+				if p.Regularizer != nil {
+					regTerm = p.Regularizer.Regularize(actor, n)
+				} else {
+					regTerm = anydiff.NewConst(c.MakeVector(n))
+				}
 
-			cm := anynet.ConcatMixer{}
-			return cm.Mix(cm.Mix(advTerm, criticTerm, n), regTerm, n)
-		},
-		actorOut,
-		criticOut,
-		lazyseq.TapeRereader(c, r.AgentOuts),
-		lazyseq.TapeRereader(c, r.Actions),
-		lazyseq.TapeRereader(c, adv),
-		lazyseq.TapeRereader(c, targetValues.Tape(c)),
-	)
-
-	objective := lazyseq.Mean(obj)
+				cm := anynet.ConcatMixer{}
+				return cm.Mix(cm.Mix(advTerm, criticTerm, n), regTerm, n)
+			},
+			actor,
+			critic,
+			lazyseq.TapeRereader(c, r.AgentOuts),
+			lazyseq.TapeRereader(c, r.Actions),
+			lazyseq.TapeRereader(c, adv),
+			lazyseq.TapeRereader(c, targetValues.Tape(c)),
+		)
+		return lazyseq.Mean(obj)
+	})
 	objective.Propagate(anyvec.Ones(c, 3), grad)
 
 	terms := &PPOTerms{
@@ -187,13 +193,34 @@ func (p *PPO) Run(r *anyrl.RolloutSet, adv lazyseq.Tape) (anydiff.Grad, *PPOTerm
 	return grad, terms
 }
 
+// runActorCritic computes the outputs of the actor and
+// the critic and passes them to f.
+// It returns the result of f.
+//
+// This may perform pooling, hence the unusual callback
+// setup.
+func (p *PPO) runActorCritic(c anyvec.Creator, r *anyrl.RolloutSet,
+	f func(actor, critic lazyseq.Rereader) anydiff.Res) anydiff.Res {
+	if p.PoolBase {
+		baseOut := p.applyBase(c, r)
+		return lazyseq.PoolToVec(baseOut, func(baseOut anyseq.Seq) anydiff.Res {
+			actorOut := p.Actor(lazyseq.Lazify(baseOut))
+			criticOut := p.Critic(lazyseq.Lazify(baseOut))
+			return f(actorOut, criticOut)
+		})
+	} else {
+		actorOut := p.Actor(p.applyBase(c, r))
+		criticOut := p.Critic(p.applyBase(c, r))
+		return f(actorOut, criticOut)
+	}
+}
+
 func (p *PPO) applyBase(c anyvec.Creator, r *anyrl.RolloutSet) lazyseq.Reuser {
 	if p.Base == nil {
 		return lazyseq.MakeReuser(lazyseq.TapeRereader(c, r.Inputs))
 	} else {
 		return lazyseq.MakeReuser(p.Base(lazyseq.TapeRereader(c, r.Inputs)))
 	}
-
 }
 
 func (p *PPO) clippedObjective(ratios, advantages anydiff.Res) anydiff.Res {
